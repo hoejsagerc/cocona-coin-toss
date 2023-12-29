3be54036645fa048b1108dc73ec0dd932a2436d4
Christian Højsager <chho@edgemo.com>
2023-12-29 14:28:22 +0100

    add object types to toss

--- src/Commands/TossCommands.cs
+++ src/Commands/TossCommands.cs
@@ -1,13 +1,43 @@
+using System.ComponentModel;
 using Cocona;
+
+
+public record CommonParameters(
+    [Option('t', Description = "Choose between: coin, boolean, life")]
+    string Type = "coin"
+) : ICommandParameterSet;
+
 
 public class TossCommands
 {
     [Command("toss")]
-    public void Toss()
+    public void Toss(CommonParameters commonParams)
     {
+        var trueObj = String.Empty;
+        var falseObj = String.Empty;
+
+        switch (commonParams.Type)
+        {
+            case "coin":
+                trueObj = "heads";
+                falseObj = "tails";
+                break;
+            case "boolean":
+                trueObj = "true";
+                falseObj = "false";
+                break;
+            case "life":
+                trueObj = "life";
+                falseObj = "death";
+                break;
+            default:
+                Console.WriteLine("Invalid object");
+                return;
+        }
+
         var random = new Random();
 
-        var coin = random.Next(0, 2) == 0 ? "heads" : "tails";
+        var coin = random.Next(0, 2) == 0 ? trueObj : falseObj;
 
         Console.WriteLine($"{coin}");
     }
